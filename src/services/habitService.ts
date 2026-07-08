d17efe11269df7a5f0aff28a54d91020692f50c2
Josephine Mundt <josephinemundt1297@gmail.com>
2026-07-08 14:49:56 +0200

    feat: update getHabitById function to fetch a single habit by ID

--- src/services/habitService.ts
+++ src/services/habitService.ts
@@ -3,11 +3,11 @@ import type { Habit } from "../types/habit";
 const API_URL = "http://localhost:3001/habits";
 // Fake API Server - npm run server
 
-export async function getHabits(): Promise<Habit[]> {
-  const response = await fetch(API_URL);
+export async function getHabitById(id: string): Promise<Habit> {
+  const response = await fetch(`${API_URL}/${id}`);
 
   if (!response.ok) {
-    throw new Error("Habits konnten nicht geladen werden.");
+    throw new Error("Habit konnte nicht geladen werden.");
   }
 
   return response.json();
