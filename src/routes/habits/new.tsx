853dfd36671d59b8d335445100d8af875f014872
AhmadNabouty <ahmad.nabouty@dci-student.org>
2026-07-09 00:17:43 +0200

    create new habit

--- src/routes/habits/new.tsx
+++ src/routes/habits/new.tsx
@@ -1,9 +1,20 @@
 import { createFileRoute } from "@tanstack/react-router";
-
+import { HabitForm } from "../../components/forms/HabitForm";
+import { defaultHabitFormValues } from "../../schemas/habitSchema";
 export const Route = createFileRoute("/habits/new")({
   component: NewHabitPage,
 });
-
 function NewHabitPage() {
-  return <h1>Create Habit Test</h1>;
+  return (
+    <div>
+      <h1>Neue Habit erstellen</h1>
+      <HabitForm
+        initialValues={defaultHabitFormValues}
+        submitLabel="habit erstellen"
+        onSubmit={(values) => {
+          console.log("neue habit:", values);
+        }}
+      />
+    </div>
+  );
 }
