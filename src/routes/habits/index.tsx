8d9dffbe9c66ccf69b60e66da52404e0e5f58fd3
AhmadNabouty <ahmad.nabouty@dci-student.org>
2026-07-10 10:30:44 +0200

    habits seite

--- src/routes/habits/index.tsx
+++ src/routes/habits/index.tsx
@@ -1,9 +1,39 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { useHabits } from "../../hooks/useHabits";
+export const Route = createFileRoute("/habits/")({
+  component: HabitOverviePage,
+});
 
-export const Route = createFileRoute('/habits/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/habits/"!</div>
+function HabitOverviePage() {
+  const { data: habits = [], isLoading, isError } = useHabits();
+  if (isLoading) {
+    return <p>Habits werden geladen...</p>;
+  }
+  if (isError) {
+    return <p>Habits konnten nicht geladen werden.</p>;
+  }
+  return (
+    <div>
+      <h1>Habits</h1>
+      <Link to="/habits/new">Neue Habit erstellen</Link>
+      {habits.length === 0 && <p>Noch keine Habits vorhanden.</p>}
+      <ul>
+        {habits.map((habit) => (
+          <li key={habit.id}>
+            <h2>{habit.title}</h2>
+            <p>{habit.description} </p>
+            <p> Kategorie: {habit.category} </p>
+            <p> Status: {habit.status} </p>
+            <p>Häufigkeit: {habit.status} </p>
+            <p> Ziel pro Woche: {habit.targetPerWeek} </p>
+            <p> Erinnerungszeit: {habit.reminderTime} </p>
+            <Link
+              to="/habits/$habitId/edit"
+              params={{ habitId: habit.id }}
+            ></Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
